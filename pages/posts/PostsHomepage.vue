4d65dea29b4c179977009dc20ac0b6f1bf466795
Hedi Hadi <84453910+hedihadi@users.noreply.github.com>
2022-04-06 23:46:45 +0300

    centering homepage posts

--- pages/posts/PostsHomepage.vue
+++ pages/posts/PostsHomepage.vue
@@ -8,55 +8,52 @@
         class="btn btn-blue"
       >New Post</router-link>
     </div>
-    <div
-      class="centered"
-      style="text-align:right;margin-right:34%"
-    >
-    </div>
     <br>
-    <div
-      v-for="post in posts"
-      :key="post.key"
-    >
+    <div class="row row-max-2">
+      <div
+        v-for="post in posts"
+        :key="post.key"
+      >
 
-      <article>
-        <div class="card card-course">
-          <div class="card-content">
-            <h4>{{post.title}}</h4>
-            <h6 style="display: inline; color: #e0e0e0">{{post.posted}}</h6>
-            <h6 style="display: inline; color: #A8A8A8">
-              ({{post.timeago}})
-            </h6>
-            <br />
-            <router-link
-              :to="'/tags/?tag=' + post.key"
-              class="tag"
-              v-for="tag in post.tags"
-              :key="tag"
-            >#{{tag}}</router-link>
-            <a
-              v-if="logged"
-              :href="'/posts/editpost?id=' + post.key"
-              class="btn btn-green"
-              style="background-color: #DAD754;color:black"
-            >Edit</a>
-            <a
-              v-if="logged"
-              @click="deletePost(post.key)"
-              class="btn btn-green"
-              style="background-color: #DA6454;;color:black"
-            >Delete</a>
-            <br>
-            <router-link
-              :to="'/posts/post?id=' + post.key"
-              class="btn btn-blue"
-              style=";color:black"
-            >Visit</router-link>
+        <article>
+          <div class="card card-course">
+            <div class="card-content">
+              <h4>{{post.title}}</h4>
+              <h6 style="display: inline; color: #e0e0e0">{{post.posted}}</h6>
+              <h6 style="display: inline; color: #A8A8A8">
+                ({{post.timeago}})
+              </h6>
+              <br />
+              <router-link
+                :to="'/tags/?tag=' + post.key"
+                class="tag"
+                v-for="tag in post.tags"
+                :key="tag"
+              >#{{tag}}</router-link>
+              <a
+                v-if="logged"
+                :href="'/posts/editpost?id=' + post.key"
+                class="btn btn-green"
+                style="background-color: #DAD754;color:black"
+              >Edit</a>
+              <a
+                v-if="logged"
+                @click="deletePost(post.key)"
+                class="btn btn-green"
+                style="background-color: #DA6454;;color:black"
+              >Delete</a>
+              <br>
+              <router-link
+                :to="'/posts/post?id=' + post.key"
+                class="btn btn-blue"
+                style=";color:black"
+              >Visit</router-link>
 
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
 
+      </div>
     </div>
     <div class="text-center">
       <router-link
